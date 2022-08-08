e7fcc70d72aa206db8273b3d108b119efe253043
isivaselvan <99784416+isivaselvan@users.noreply.github.com>
2022-08-08 17:43:34 +0530

    Update vpc_subnet.tf

--- vpc_subnet.tf
+++ vpc_subnet.tf
@@ -45,3 +45,50 @@ resource "aws_route_table_association" "web_subnet_association" {
   subnet_id      = aws_subnet.web_subnet.id
   route_table_id = aws_route_table.web_route_table.id
 }
+
+# 6. create security group to allow port 22, 80, 443  
+resource "aws_security_group" "web_allow_http_ssh" {
+  name        = "allow_web_ssh"
+  description = "Allow wen and ssh inbound traffic"
+  vpc_id      = aws_vpc.web_vpc.id
+
+  ingress {
+    description      = "HTTPS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "ssh from VPC"
+    from_port        = 20
+    to_port          = 20
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web_ssh"
+  }
+}  
+  
